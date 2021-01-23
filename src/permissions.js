1e8d050ef2d482a11eac58e4dfcaf0f607b59b7f
Pedro Borges <pedro.hov.borges@gmail.com>
2021-01-23 00:02:57 -0300

    update ismember check

--- src/permissions.js
+++ src/permissions.js
@@ -1,5 +1,6 @@
 module.exports = {
     isUserMember(user, config) {
+        if (!user) return false;
         return user.roles.cache.find(role => role.id === config.roles.memberRoleID);
     }
 }
